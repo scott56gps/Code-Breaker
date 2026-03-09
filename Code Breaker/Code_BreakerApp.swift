52935a67785ff397eeee0036917de59a7d3d32bd
Scott Nicholes <scott93gps@gmail.com>
2026-03-09 15:57:20 -0700

    Added Xcode Project

---
+++ Code Breaker/Code_BreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_BreakerApp.swift
+//  Code Breaker
+//
+//  Created by Scott Nicholes on 3/9/26.
+//
+
+import SwiftUI
+
+@main
+struct Code_BreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Code BreakerTests/Code_BreakerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Code_BreakerTests.swift
+//  Code BreakerTests
+//
+//  Created by Scott Nicholes on 3/9/26.
+//
+
+import Testing
+@testable import Code_Breaker
+
+struct Code_BreakerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
